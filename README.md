3ecd9d09698a0eef38888d780fcaa7028d170322
unknown <michaelwojones@gmail.com>
2018-09-20 18:45:52 +1000

    Added path variable based service

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Example Spring Boot Project
 Quick example to use as a template as needed.
+
+## Command to run project
+mvn package && java -jar target/spring.boot.tutorial-1.0.0.jar
--- src/main/java/spring/boot/tutorial/controllers/DriverManagerController.java
+++ src/main/java/spring/boot/tutorial/controllers/DriverManagerController.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +25,13 @@ public class DriverManagerController {
 
 		String query = "SELECT * FROM TEST_SCHEMA.BOOKS";
 		BookBean book = new BookBean();
-		
+
 		try {
 			Class.forName("org.h2.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
-		} 
-		
+		}
+
 		try (Connection con = DriverManager.getConnection(_URL, _USER, _PW);
 				Statement st = con.createStatement();
 				ResultSet rs = st.executeQuery(query)) {
@@ -41,6 +42,34 @@ public class DriverManagerController {
 				book.setAuthor(rs.getString(3));
 				System.out.printf("%d %s %s%n", rs.getInt(1), rs.getString(2), rs.getString(3));
 			}
+
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		}
+
+		return book;
+	}
+
+	@RequestMapping(path = "/books/{bookId}", method = RequestMethod.GET)
+	public BookBean getSpecificBook(@PathVariable int bookId) {
+
+		BookBean book = new BookBean();
+		String query = "SELECT * FROM TEST_SCHEMA.BOOKS WHERE ID=" + bookId;
+
+		try {
+			Class.forName("org.h2.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		try (Connection con = DriverManager.getConnection(_URL, _USER, _PW);
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(query)) {
+
+			rs.next();
+			book.setId(rs.getInt(1));
+			book.setTitle(rs.getString(2));
+			book.setAuthor(rs.getString(3));
 
 		} catch (SQLException ex) {
 			throw new RuntimeException(ex);
